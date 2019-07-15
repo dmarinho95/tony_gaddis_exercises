e8049ef44b4b6eef1c655d068f7b54e136e343d6
unknown <dmarinho95@gmail.com>
2019-07-15 01:37:40 -0400

    sentencecapitalizer refactored

--- src/refresher/sentenceCapitalizer.java
+++ src/refresher/sentenceCapitalizer.java
@@ -1,61 +1,42 @@
 package refresher;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 
-
-public class sentenceCapitalizer
+public class sentencecapitalizer 
 {
-	public static char [] sentenceCapitalizer(String str)
-	{
-		
-		ArrayList <Character> char_array = new ArrayList<Character>();
-		ArrayList <Character> char_array2 = new ArrayList<Character>();
-		for(int i=0; i<=str.length()-1;i++)
-		{
-		char_array.add(str.charAt(i));
-    	}
+	String sentence;
 	
-		for(int i=0 ; i<str.length()-1;i++)
-		{
-		   if(char_array.get(i)=='.')
-		   {
-			 char_array2.add(Character.toUpperCase(char_array.get(i+2)));
-		   }
-		   
-		}
-		for(int j=0; j<char_array2.size();j++)
-		{
-			for(int i=0 ; i<str.length()-1;i++)
-			{
-			if(Character.toLowerCase(char_array.get(i))==(Character.toLowerCase(char_array2.get(j)))&&char_array.get(i-2)=='.')
-						{
-			                  char_array.set(i, char_array2.get(j));
-		
-						}
-			
-			}
-		
-		}
-		
-	    char [] char_array_formatted = new char [char_array.size()];
-	    char_array_formatted = char_array.toString().replace(",", " ").replace("[", " ").replace("]", " ").toCharArray(); 
-		
-		return char_array_formatted;
-		
+	public sentencecapitalizer (String sentence)
+	{
+		this.sentence = sentence;
+	}
+
+	
+	public void sentencecapitalizer()
+	{
+	  ArrayList <Character> arraylist = new ArrayList <Character>();
+	  for(int i=0; i<=sentence.length()-1;i++)
+	  {
+		  arraylist.add(sentence.charAt(i));
+      }
+	  for(int i=0; i<=arraylist.size()-1;i++)
+	  {
+		  arraylist.set(0, Character.toUpperCase(arraylist.get(0)));
+		  if(arraylist.get(i)=='.' && Character.isLowerCase(arraylist.get(i+2)))
+		  {
+			  arraylist.set(i+2, Character.toUpperCase(arraylist.get(i+2)));
+		  }
+	  }
+	  System.out.println(arraylist);
 	}
 	
-	public static void main (String [] args)
+	public static void main (String [] args) 
 	{
-		String str = "Aye yo. whats up my name is Marinho.";
-		System.out.println(sentenceCapitalizer.sentenceCapitalizer(str));
-	
+		String sentence = "hello. my name is Joe. what is your name ?";
+		sentencecapitalizer myformatter = new sentencecapitalizer (sentence);
+		myformatter.sentencecapitalizer();
+		
 	}
-	
-	
 }
-			
-	
-
-	
-
